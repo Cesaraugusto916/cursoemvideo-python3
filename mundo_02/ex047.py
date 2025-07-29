d2888647d0e38421f3c04799a72b94dceab85406
Cesaraugusto916 <cesar916augusto@gmail.com>
2025-07-29 15:54:48 -0300

    chore: remove unused time import

--- mundo_02/ex047.py
+++ mundo_02/ex047.py
@@ -4,8 +4,6 @@ EXERCÍCIO 047: Contagem de Pares
 Crie um programa que mostre na tela todos os números pares que estão no intervalo entre 1 e 50.
 """
 
-import time
-
 print('Esses são os numeros pares de 1 a 50:')
 for c in range(2, 51, 2):
     print(c, end=' ')
