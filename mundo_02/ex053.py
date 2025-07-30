2ea9064124a21e951eeee4d9304dabe1b7bb0fea
Cesaraugusto916 <cesar916augusto@gmail.com>
2025-07-30 15:18:29 -0300

    refactor: change input normalization from lower to upper

--- mundo_02/ex053.py
+++ mundo_02/ex053.py
@@ -4,8 +4,8 @@ EXERCÍCIO 053: Detector de Palíndromo
 Crie um programa que leia uma frase qualquer e diga se ela é um palíndromo, desconsiderando os espaços.
 """
 
-frase = input("Digite uma frase: ").replace(" ", "").lower()
-print(f"O inverso de: {frase.upper()} é\n {frase[::-1].upper()}")
+frase = input("Digite uma frase: ").replace(" ", "").upper()
+print(f"O inverso de: {frase} é\n {frase[::-1]}")
 palindromo = frase == frase[::-1]
 if palindromo:
     print("A frase é um palíndromo!")
