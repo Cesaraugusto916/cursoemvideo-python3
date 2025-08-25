b1a25e98a08bb1af9d90d4363488c70fd688736a
Cesaraugusto916 <cesar916augusto@gmail.com>
2025-08-25 15:05:29 -0300

    feat: refactor guessing game and menu program for improved functionality and clarity

--- mundo_02/ex058.py
+++ mundo_02/ex058.py
@@ -5,8 +5,18 @@ Melhore o jogo do EXERCÍCIO 028 onde o computador vai "pensar" em um número en
 Só que agora o jogador vai tentar adivinhar até acertar, mostrando no final quantos
 palpites foram necessários para vencer.
 """
-n = int(input('Digite um número inteiro: '))
-nn = 0
-while n != 0:
-    nn = n
-    n -= 1
+
+from random import randint
+n_pc = randint(0, 10)
+print('Olá, usuário. Eu acabei de pensar em um número de 0 a 10!')
+n_us = int(input('Qual número eu pensei?: '))
+tent = 1
+
+while n_us != n_pc:
+    tent += 1
+    if n_us > n_pc:
+        print('Menos...')
+    else:
+        print('Mais...')
+    n_us = int(input('Tente novamente: '))
+print(f'Parabéns! Eu estava pensando no {n_pc}.\nVocê demorou {tent} tentativas.')
--- mundo_02/ex059.py
+++ mundo_02/ex059.py
@@ -1,19 +1,47 @@
+"""
+EXERCÍCIO 059: Criando um Menu de Opções
+
+Crie um programa que leia dois valores e mostre um menu como o abaixo na tela:
+
+[ 1 ] Somar
+[ 2 ] Multiplicar
+[ 3 ] Maior
+[ 4 ] Novos Números
+[ 5 ] Sair do Programa
+
+Seu programa deverá realizar a operação solicitada em cada caso.
+"""
+
+from time import sleep
 n1 = int(input('Insira um valor inteiro: '))
 n2 = int(input('Insira outro valor inteiro: '))
+op = 0
 
-q = int(input('Selecione uma opção:' \
-'\n[1] para somar' \
-'\n[2] para multiplicar' \
-'\n[3] para mostrar o maior' \
-'\n[4] para selecionar novos números' \
-'\n[5] para sair do programa'))
+while op != 5:
+    print("""Selecione uma opção:
+    [ 1 ] para somar
+    [ 2 ] para multiplicar
+    [ 3 ] para mostrar o maior
+    [ 4 ] para selecionar novos números
+    [ 5 ] para sair do programa""")
+    op = int(input('>>>>> Qual a sua opção? '))
+    if op == 1:
+        print(f'{n1} + {n2} = {n1 + n2}')
+    elif op == 2:
+        print(f'{n1} * {n2} = {n1 * n2}')
+    elif op == 3:
+        if n1 > n2:
+            print(f'Entre {n1} e {n2}, {n1} é o maior número.')
+        else:
+            print(f"Entre {n1} e {n2}, {n2} é o maior número.")
+    elif op == 4:
+        print('Informe os números novamente:')
+        n1 = int(input('Insira um valor inteiro: '))
+        n2 = int(input('Insira outro valor inteiro: '))
+    else:
+        print('Opção inválida. Tente novamente')
+    print('=-=' * 15)
+    sleep(2)
 
-while q != 5:
-    q = int(input('Selecione uma opção:' \
-'\n[1] para somar' \
-'\n[2] para multiplicar' \
-'\n[3] para mostrar o maior' \
-'\n[4] para selecionar novos números' \
-'\n[5] para sair do programa'))
-    
+
 print('Fim do programa')
