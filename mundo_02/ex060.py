b27b4561688ab116a1ca64477eae7fecd8916422
Cesaraugusto916 <cesar916augusto@gmail.com>
2025-08-25 15:23:05 -0300

    feat: enhance fatorial calculation program with improved structure and clarity

--- mundo_02/ex060.py
+++ mundo_02/ex060.py
@@ -1,5 +1,22 @@
+"""
+EXERCÍCIO 060: Cálculo do Fatorial
+
+Faça um programa que leia um número qualquer e mostre seu fatorial.
+
+Ex: 5! = 5 x 4 x 3 x 2 x 1 = 120
+"""
+
+from time import sleep
 n = int(input('Digite um número inteiro: '))
-nn = 0
-while n != 0:
-    nn = n
-    n -= 1
+c = n
+f = 1
+
+print(f'Calculando {n}!')
+sleep(2)
+while c > 0:
+    print(f'{c}', end = '')
+    print(' x ' if c > 1 else ' = ', end = '') 
+    f *= c
+    c -= 1
+print(f'{f}')
+sleep(5)
