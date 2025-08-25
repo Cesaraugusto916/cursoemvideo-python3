e75c5f7d6840d987ebcdb5221744d9784fad368e
Cesaraugusto916 <cesar916augusto@gmail.com>
2025-08-25 14:42:18 -0300

    feat: refactor guessing game to implement countdown mechanism

--- mundo_02/ex058.py
+++ mundo_02/ex058.py
@@ -5,24 +5,8 @@ Melhore o jogo do EXERCÍCIO 028 onde o computador vai "pensar" em um número en
 Só que agora o jogador vai tentar adivinhar até acertar, mostrando no final quantos
 palpites foram necessários para vencer.
 """
-
-import random
-
-c = 1
-n_pc = random.randint(0,10)
-print('Olá, usuário. Eu acabei de pensar em um número de 0 a 10!')
-
-acertou = False
-palpites = 0
-
-while not acertou:
-    n_us = int(input('Qual número eu pensei?: '))
-    palpites += 1
-    if n_us == n_pc:
-        acertou = True
-    else:
-        if n_us > n_pc:
-            print('Menos')
-        else:
-            print('Mais')
-print(f'Você acertou com {palpites} tentativas!')
+n = int(input('Digite um número inteiro: '))
+nn = 0
+while n != 0:
+    nn = n
+    n -= 1
