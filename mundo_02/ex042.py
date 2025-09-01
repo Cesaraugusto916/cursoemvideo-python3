b0f031ff37ac1816f8ce71da31f6d724495f771e
Cesaraugusto916 <cesar916augusto@gmail.com>
2025-09-01 14:51:03 -0300

    refactor(mundo_02/ex042.py): Simplify triangle type check
    - Improved code readability.
    - Removed unnecessary variable assignments.
    - Simplified triangle type determination.
    - Used more concise conditional logic.
    - Optimized input prompts.

--- mundo_02/ex042.py
+++ mundo_02/ex042.py
@@ -8,34 +8,17 @@ Refaça o EXERCÍCIO 035 dos triângulos, acrescentando o recurso de mostrar que
 - Escaleno: todos os lados diferentes
 """
 
-r1 = float(input('Digite o valor de uma reta: '))
-r2 = float(input('Digite o valor de outra reta: '))
-r3 = float(input('Digite o valor da última reta: '))
+r1 = float(input('Primeiro segmento: '))
+r2 = float(input('Segundo segmento: '))
+r3 = float(input('Terceiro segmento: '))
 
-if (r1 <= r2) and (r1 <= r3):
-    menor = r1
-elif (r2 <= r1) and (r2 <= r3):
-    menor = r2
+if (r1 < r2 + r3) and (r2 < r1 + r3) and (r3 < r1 + r2):
+    print('Os segmentos acima podem formar um triângulo ', end='')
+    if r1 == r2 == r3:
+        print('EQUILÁTERO.')
+    elif r1 == r2 or r2 == r3 or r1 == r3:
+        print('ISÓSCELES.')
+    else:
+        print('ESCALENO.')
 else:
-    menor = r3
-
-if (r1 >= r2) and (r1 >= r3):
-    maior = r1
-elif (r2 >= r1) and (r2 >= r3):
-    maior = r2
-else:
-    maior = r3
-
-meio = (r1 + r2 + r3) - maior - menor
-
-if menor + meio > maior:
-    print(f'Os seguimentos {menor}, {meio} e {maior} podem formar triangulo!')
-else:
-    print(f'Os seguimentos {menor:}, {meio:} e {maior} NÃO podem formar triangulo')
-
-if menor == meio and meio == maior:
-    print('Seu triângulo será EQUILÁTERO!')
-elif menor == meio or menor == maior or meio == maior:
-    print('Seu triângulo será ISÓCELER!')
-else:
-    print('Seu triângulo será ESCALENO!')
+    print('Os segmentos acima não podem formar um triângulo.')
