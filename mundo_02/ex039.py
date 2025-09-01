cc2972a8ed76c3803acbb34ad4abf52da457d0f8
Cesaraugusto916 <cesar916augusto@gmail.com>
2025-09-01 15:12:14 -0300

    fix(mundo_02): update exercises status and code
    - Updated `ex039.py` and `ex041.py` to use current year.
    - Improved `ex041.py` age classification logic.
    - Corrected exercise statuses in `exercicios.txt`.
    - Minor fixes and improvements to code logic.
    - Addressed issues in several exercises.

--- mundo_02/ex039.py
+++ mundo_02/ex039.py
@@ -6,11 +6,14 @@ se ele ainda vai se alistar ao serviço militar, se é a hora de se alistar, ou 
 do tempo do alistamento. Seu programa também deverá mostrar o tempo que falta ou que passou do prazo.
 """
 
+from datetime import datetime
+
+ano_atual = datetime.now().year
 ano = int(input('Digite seu ano de nascimento: '))
 
-if (2025 - ano) > 18:
-    print(f'Você passou {(2025-ano)-18} ano(s) do tempo de se alistar!')
-elif (2025 - ano) == 18:
+if (ano_atual - ano) > 18:
+    print(f'Você passou {(ano_atual-ano)-18} ano(s) do tempo de se alistar!')
+elif (ano_atual - ano) == 18:
     print('Você está na idade de se alistar!')
 else:
-    print(f"Ainda falta {18-(2025-ano)} ano(s) para o seu alistamento!")
+    print(f"Ainda falta {18-(ano_atual-ano)} ano(s) para o seu alistamento!")
--- mundo_02/ex041.py
+++ mundo_02/ex041.py
@@ -11,19 +11,21 @@ de nascimento de um atleta e mostre sua categoria, de acordo com a idade:
 - Acima: MASTER
 """
 
+
+
 ano = int(input('Digite o ano de nascimento: '))
 idade = 2025 - ano
 print(f'Sua idade: {idade:.0f}')
 
-if idade < 9:
-    print('Categoria: MIRIM')
-elif idade >= 9 and idade < 14:
-    print('Categoria: INFANTIL')
-elif idade >= 14 and idade < 19:
-    print('Categoria: JUNIOR')
-elif idade >= 19 and idade < 20:
-    print('Categoria: SÊNIOR')
-elif idade >= 20:
-    print('Categoria: MASTER')
+print(f'O atleta tem {idade} anos.')
+
+if idade <= 9:
+    print('Classificação: MIRIM')
+elif idade <= 14:
+    print('Classificação: INFANTIL')
+elif idade <= 19:
+    print('Classificação: JUNIOR')
+elif idade <= 25:
+    print('Classificação: SÊNIOR')
 else:
-    print('Idade inválida')
+    print('Classificação: MASTER')
