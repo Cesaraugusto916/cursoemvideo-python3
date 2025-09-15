f039a7d35cd5bad96991eaca472840b68f4e69c7
Cesaraugusto916 <cesar916augusto@gmail.com>
2025-09-15 16:35:28 -0300

    feat: add program to sum integers until 999

---
+++ mundo_02/ex064.py
@@ -0,0 +1,18 @@
+"""
+EXERCÍCIO 064: Tratando Vários Valores v1.0
+
+Crie um programa que leia vários números inteiros pelo teclado. O programa só vai parar quando o usuário digitar
+o valor 999, que é a condição de parada. No final, mostre quantos números foram digitados e qual foi a soma
+entre eles (desconsiderando o flag).
+"""
+
+n = s = c = 0
+
+n = int(input('Digite um número inteiro [999 para parar]: '))
+
+while n != 999:
+    s += n
+    c += 1
+    n = int(input('Digite um número inteiro [999 para parar]: '))
+print(f'Você digitou {c} números e a soma entre eles foi {s}.')
+print('Fim do programa!')
